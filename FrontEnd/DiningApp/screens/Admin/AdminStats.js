26925051c87a8d449d99969ea80d832d3a4419d2
donguyen6c <donguyen6c@gmail.com>
2026-05-08 11:25:57 +0700

    update FE

---
+++ FrontEnd/DiningApp/screens/Admin/AdminStats.js
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import Styles from "../../styles/Styles";
+
+const AdminStats = () => {
+    return (
+        <Text style={Styles.subject}>QUAN LY FOOD</Text>
+    );
+}
+
+export default AdminStats;
